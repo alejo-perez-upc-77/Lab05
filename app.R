bd2495adccb5334e3dae9483844171341df37a89
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-09-30 14:39:16 +0200

    Apply the same logic in RenderText reactive method

--- app.R
+++ app.R
@@ -44,7 +44,6 @@ server <- function(input, output) {
   # This expression that generates a histogram is wrapped in a call
   # to renderPlot to indicate that:
   #
-  # 1. It is "reactive" and therefore should be automatically
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$value <- renderText({ input$location })
